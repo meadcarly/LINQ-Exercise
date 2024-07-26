15298bf4f917a14be289a9d360da99e34634c7c3
Carly Mead <meadcarly16@gmail.com>
2024-07-26 16:17:04 -0600

    Created a list and ordered it by name length and printed it to the console

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create a list of video game names...
+            var videoGameNames = new List<string>() { "Mario Cart", "Rayman", "Crash B.", "Halo" };
+            
+            //Order the list of games by length of the game name.
+            var byNameLength = videoGameNames.OrderBy(x => x.Length).ToList();
+
+            byNameLength.ForEach(x => Console.WriteLine(x));
+
         }
     }
 }
